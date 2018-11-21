b53aa4eb12dede99c9389b67d882fb50ec2bb1b2
Vincent.Chan <vincent.chan@COMM-VCHAN-MAC.local>
2018-11-20 22:11:18 -0500

    Initial commit for BnB

---
+++ test/property.js
@@ -0,0 +1,143 @@
+//jshint ignore: start
+
+// contracts
+const Property = artifacts.require("./Property.sol");
+const PropertyRegistry = artifacts.require("./PropertyRegistry.sol");
+//var BigNumber = require('bignumber.js');
+
+/**************************************
+* Tests
+**************************************/
+
+contract('Property Contract Tests', function(accounts) {
+
+  let myNewToken;
+  let originalBalance;
+  let secondBalance;
+  let property;
+  let propertyRegistry;
+  let amountToSend = 250;
+  const owner = accounts[0]; 
+  const alice = accounts[1];
+  const third = accounts[2];
+  const bob = accounts[3];
+  const eve = accounts[4];
+
+  it('should be deployed, Property', async () => {
+    property = await Property.deployed();
+    assert(property !== undefined, 'Property was NOT deployed' + property);
+  });
+
+  it('It should allow Alice to createProperty()', async () => {
+      let tx;
+    try {
+        tx = await property.createProperty({from: alice});
+    } catch(e) {
+      assert(false, 'Alice could not use createProperty()');
+      return;
+    }
+    assert(true, 'Alice could use createProperty()');
+  });
+
+  let ownedTokenId;
+
+  it('Alice should get a unique tokenId', async () => {
+    ownedTokenId = await property.tokenOfOwnerByIndex(alice, 0, {from: alice});
+    assert(ownedTokenId, 'Alice does not have a valid tokenId');
+  });
+
+  it('She should be able to setURI(herTokenId) to some string', async () => {
+    try {
+      await property.setURI(ownedTokenId, 'someString', {from: alice});
+    } catch(e) {
+      assert(false, 'Alice could not use setURI(). error: ' + e);
+      return;
+    }
+    assert(true, 'Alice could use setURI()');
+  });
+
+  it('It should not allow someone other than Alice to setURI(herTokenId)', async () => {
+    try {
+      await property.setURI(ownedTokenId, 'someOtherString', {from: third});
+    } catch(e) {
+      assert(true, 'Someone other than Alice could not use setURI(). error: ' + e);
+      return;
+    }
+    assert(false, 'Someone other than Alice could use setURI()');
+  });
+
+  it('should only allow the owner of a property to register that property', async () => {
+    try {
+      await propertyRegistry.registerProperty(ownedTokenId, 50, {from: alice});
+    } catch(e) {
+      assert(true, 'Someone other than Alice could not register that property. error: ' + e);
+      return;
+    }
+    assert(false, 'Someone other than Alice could register that property');
+  });
+
+  it('should be deployed, PropertyRegistry', async () => {
+    propertyRegistry = await PropertyRegistry.deployed();
+    assert(propertyRegistry !== undefined, 'PropertyRegistry was NOT deployed' + propertyRegistry);
+  });
+
+  it('Bob can submit a request', async () => {
+    try {
+      await propertyRegistry.request(ownedTokenId, 1542603516, 1543121916, {from: bob});
+    } catch(e) {
+      assert(false, 'Bob could not submit a request. error: ' + e);
+      return;
+    }
+    assert(true, '');
+  });
+
+  it('Eve should not be able to submit another request after Bob did', async () => {
+    try {
+      await propertyRegistry.request(ownedTokenId, 1542603516, 1543121916, {from: eve});
+    } catch(e) {
+      assert(true, e);
+      return;
+    }
+    assert(false, 'Eve was able to submit another request.');
+  });
+
+  it('Bob can only check-in after Alice has approved and the approved time has come.', async () => {
+    try {
+      await propertyRegistry.checkIn(ownedTokenId, {from: bob});
+    } catch(e) {
+      assert(true, e);
+      return;
+    }
+    assert(false, 'Bob was able to check in.');
+  });
+
+  it('After Alice has approved the request, Bob can check-in.', async () => {
+    try {
+      await propertyRegistry.approveRequest(ownedTokenId, {from: alice});
+      await propertyRegistry.checkIn(ownedTokenId, {from: bob});
+    } catch(e) {
+      assert(false, 'Bob was not able to check in after Alice approved the request.' + e);
+      return;
+    }
+    assert(true, 'Bob was able to check in after Alice approved the request.');
+  });
+
+  it('A new request can be submitted by other guests.', async () => {
+    try {
+      await propertyRegistry.request(ownedTokenId, 1542603516, 1543121916, {from: eve});
+    } catch(e) {
+      assert(true, 'A new request could not be submitted' + e);
+      return;
+    }
+    assert(true, 'Eve was able to submit another request.');
+  });
+
+
+  // Bob can submit a request
+  // Eve should not be able to submit another request after Bob did
+  // Alice can approve Bob's request (or not)
+  // Eve cannot check-in into the property, even if the correct time has come.
+  // Bob can only check-in after Alice has approved and the approved time has come.
+  // Bob can check-out
+  // A new request can be submitted by other guests
+});
---
+++ test/propertyregistry.js
@@ -0,0 +1,123 @@
+// contracts
+const Property = artifacts.require("./Property.sol");
+const PropertyRegistry = artifacts.require("./PropertyRegistry.sol");
+const PropertyToken = artifacts.require("./PropertyToken.sol");
+
+/**************************************
+* Tests
+**************************************/
+
+contract('Property Token Contract Tests', function(accounts) {
+
+  const alice = accounts[0]
+  const bob = accounts[1];
+
+  const allocation = 10000;
+  it('should be deployed, Property', async () => {
+    property = await Property.deployed();
+    assert(property !== undefined, 'Property was NOT deployed' + property);
+  });
+
+  it('should be deployed, Property Registry', async () => {
+    propertyRegistry = await PropertyRegistry.deployed();
+    assert(propertyRegistry !== undefined, 'propertyRegistry was NOT deployed' + propertyRegistry);
+  });
+
+  it('should be deployed, Property Token', async () => {
+    propertyToken = await PropertyToken.deployed();
+    assert(propertyToken !== undefined, 'PropertyToken was NOT deployed' + propertyToken);
+  });
+
+  it('should allow alice to mint Property Token for bob', async () => {
+    const tx = await propertyToken.mint(bob, allocation);
+    //get the balance of property tokens for bob
+    const balance = await propertyToken.balanceOf.call(bob);
+    assert.equal(balance.toNumber(), allocation, 'balance');
+  });
+
+  const price = 1000;
+  it('should allow bob to approve the property registry to use his tokens', async () => {
+    try {
+      const tx = await propertyToken.approve(propertyRegistry.address, price, { from: bob });
+    } catch (e) {
+      assert.equal(tx, undefined, 'property registry has not been approved. error: ' + e);
+      return;
+    }
+  });
+
+  it('It should allow Alice to createProperty()', async () => {
+    let tx;
+    try {
+        tx = await property.createProperty({from: alice});
+    } catch(e) {
+      assert(false, 'Alice could not use createProperty()');
+      return;
+    }
+  });
+
+  let ownedTokenId; 
+
+  it('Alice should get a unique tokenId', async () => {
+    ownedTokenId = await property.tokenOfOwnerByIndex(alice, 0, {from: alice});
+    assert(ownedTokenId, 'Alice does not have a valid tokenId');
+  });
+
+  let propertyPrice = 50;
+
+  it('Alice should be able to register that property', async () => {
+    try {
+      await propertyRegistry.registerProperty(ownedTokenId, propertyPrice, {from: alice});
+    } catch(e) {
+      assert(false, 'Alice could not register that property. error: ' + e);
+      return;
+    }
+  });
+
+  it('Bob can submit a request', async () => {
+    try {
+      await propertyRegistry.request(ownedTokenId, 1542603516, 1543121916, {from: bob});
+    } catch(e) {
+      assert(false, 'Bob could not submit a request. error: ' + e);
+      return;
+    }
+  });
+
+  it('Alice should be able to approve the request.', async () => {
+    try {
+      await propertyRegistry.approveRequest(ownedTokenId, {from: alice});
+    } catch(e) {
+      console.log(e);
+      assert(false, 'Alice could not approve the request.' + e);
+      return;
+    }
+  });
+
+  let preCheckInBalance; 
+  it('After Alice has approved the request, Bob can check-in.', async () => {
+    try {
+      preCheckInBalance = await propertyToken.balanceOf.call(alice);
+      await propertyRegistry.checkIn(ownedTokenId, {from: bob});
+    } catch(e) {
+      console.log(e);
+      assert(false, 'Bob was not able to check in.' + e);
+      return;
+    }
+  });
+
+  it('Bob can check-out.', async () => {
+    try {
+      await propertyRegistry.checkOut(ownedTokenId, {from: bob});
+    } catch(e) {
+      console.log(e);
+      assert(false, 'Bob was not able to check out.' + e);
+      return;
+    }
+  });
+  
+  it('Verify Alice has been paid.', async () => {
+    let postcheckInBalance = await propertyToken.balanceOf.call(alice);
+    propertyPrice
+    assert.equal(postcheckInBalance.toNumber(), preCheckInBalance.toNumber() + propertyPrice, "Alice has been paid the property price");
+  });
+
+});
